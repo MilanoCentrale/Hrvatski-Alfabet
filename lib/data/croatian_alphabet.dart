4837a3960967edf09711985068f58be02a156c96
MilanoCentrale <37938605+MilanoCentrale@users.noreply.github.com>
2026-03-27 10:46:20 +0100

    Create croatian_alphabet.dart

---
+++ lib/data/croatian_alphabet.dart
@@ -0,0 +1,48 @@
+// lib/data/croatian_alphabet.dart
+
+class LetterData {
+  final String upper;
+  final String lower;
+  final String ipa;
+  final String audioUrl;
+
+  const LetterData({
+    required this.upper,
+    required this.lower,
+    required this.ipa,
+    required this.audioUrl,
+  });
+}
+
+const List<LetterData> croatianAlphabet = [
+  LetterData(upper: 'A', lower: 'a', ipa: '/a/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Hr-A.ogg'),
+  LetterData(upper: 'B', lower: 'b', ipa: '/b/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/Hr-B.ogg'),
+  LetterData(upper: 'C', lower: 'c', ipa: '/ts/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Hr-C.ogg'),
+  LetterData(upper: 'Č', lower: 'č', ipa: '/tʃ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Hr-%C4%8C.ogg'),
+  LetterData(upper: 'Ć', lower: 'ć', ipa: '/tɕ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Hr-%C4%86.ogg'),
+  LetterData(upper: 'D', lower: 'd', ipa: '/d/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Hr-D.ogg'),
+  LetterData(upper: 'Dž', lower: 'dž', ipa: '/dʒ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Hr-D%C5%BE.ogg'),
+  LetterData(upper: 'Đ', lower: 'đ', ipa: '/dʑ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Hr-%C4%90.ogg'),
+  LetterData(upper: 'E', lower: 'e', ipa: '/e/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Hr-E.ogg'),
+  LetterData(upper: 'F', lower: 'f', ipa: '/f/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Hr-F.ogg'),
+  LetterData(upper: 'G', lower: 'g', ipa: '/ɡ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Hr-G.ogg'),
+  LetterData(upper: 'H', lower: 'h', ipa: '/x/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Hr-H.ogg'),
+  LetterData(upper: 'I', lower: 'i', ipa: '/i/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Hr-I.ogg'),
+  LetterData(upper: 'J', lower: 'j', ipa: '/j/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Hr-J.ogg'),
+  LetterData(upper: 'K', lower: 'k', ipa: '/k/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Hr-K.ogg'),
+  LetterData(upper: 'L', lower: 'l', ipa: '/l/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Hr-L.ogg'),
+  LetterData(upper: 'Lj', lower: 'lj', ipa: '/ʎ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Hr-LJ.ogg'),
+  LetterData(upper: 'M', lower: 'm', ipa: '/m/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Hr-M.ogg'),
+  LetterData(upper: 'N', lower: 'n', ipa: '/n/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Hr-N.ogg'),
+  LetterData(upper: 'Nj', lower: 'nj', ipa: '/ɲ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hr-NJ.ogg'),
+  LetterData(upper: 'O', lower: 'o', ipa: '/o/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/19/Hr-O.ogg'),
+  LetterData(upper: 'P', lower: 'p', ipa: '/p/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Hr-P.ogg'),
+  LetterData(upper: 'R', lower: 'r', ipa: '/r/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Hr-R.ogg'),
+  LetterData(upper: 'S', lower: 's', ipa: '/s/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Hr-S.ogg'),
+  LetterData(upper: 'Š', lower: 'š', ipa: '/ʃ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Hr-%C5%A0.ogg'),
+  LetterData(upper: 'T', lower: 't', ipa: '/t/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/00/Hr-T.ogg'),
+  LetterData(upper: 'U', lower: 'u', ipa: '/u/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/54/Hr-U.ogg'),
+  LetterData(upper: 'V', lower: 'v', ipa: '/ʋ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9b/Hr-V.ogg'),
+  LetterData(upper: 'Z', lower: 'z', ipa: '/z/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Hr-Z.ogg'),
+  LetterData(upper: 'Ž', lower: 'ž', ipa: '/ʒ/', audioUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Hr-%C5%BD.ogg'),
+];
